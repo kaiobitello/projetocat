70dee586f7b5f4ef1b14af774c69ab4f34d9e790
Kaiozxc <kaiuz0000000@gmail.com>
2025-04-10 23:40:32 -0300

    render

--- app.js
+++ app.js
@@ -62,7 +62,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', admin)
 
 //Resto;
-const PORT = 2903
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta: http://localhost:${PORT}`)
-})
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log("Servidor rodando na porta", PORT))
