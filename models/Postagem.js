59f31edee40d25dffbca6fb87da424e552f99782
Kaiozxc <kaiuz0000000@gmail.com>
2025-04-10 22:58:08 -0300

    Sistema de Postagens

---
+++ models/Postagem.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema
+
+const Postagem = new Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    descricao: {
+        type: String,
+        required: true
+    },
+    conteudo: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: Schema.Types.ObjectId,
+        ref: "categorias",
+        required: true
+    }
+})
+
+mongoose.model("postagens", Postagem)
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,10 @@ import express from 'express'
 const router = express.Router()
 import mongoose from 'mongoose'
 import '../models/Categoria.js'
+import '../models/Postagem.js'
 const Categoria = mongoose.model("categorias")
+const Postagem = mongoose.model("postagens")
+
 
 router.get('/',(req,res) => {
     res.render('admin/index.handlebars')
@@ -69,11 +72,11 @@ router.post("/categorias/nova",(req, res) => {
 
         var erros = []
 
-    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null || req.body.nome.trim().length > 3) {
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null || req.body.nome.trim().length < 3) {
         erros.push({texto: "Nome Inválido!"})
     }
 
-    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null || req.body.slug.trim().length > 3){
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null || req.body.slug.trim().length < 3){
         erros.push({texto: "Slug Inválido!"})
     }
 
@@ -110,4 +113,74 @@ router.post("/categorias/nova",(req, res) => {
 
     })
     
+    router.get("/postagens", (req, res) => {
+        Postagem.find().populate("categoria").then((postagens) => {
+            res.render('admin/postagens', {postagens})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao carregar as postagens, tente novamente.")
+        })
+    })
+
+    router.get("/postagens/add", (req, res) => {
+    
+        Categoria.find().then((categorias) => {
+            res.render("admin/addpostagem", {categorias: categorias})
+        }).catch((err) => {
+            console.log("Ocorreu um erro! "+err)
+            req.flash("error_msg", "Ocorreu um erro ao abrir a página, tente novamente.")
+            res.redirect("/postagens")
+        })
+    })
+
+
+    router.post("/postagens/nova", (req, res) => {
+
+        var erros = []
+
+        if (!req.body.titulo || typeof req.body.titulo == undefined || req.body.titulo == null || req.body.titulo.trim().length < 3) {
+            erros.push({texto: "Nome Inválido!"})
+        }
+    
+        if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null || req.body.slug.trim().length < 3){
+            erros.push({texto: "Slug Inválido!"})
+        }
+    
+        if (!req.body.descricao || typeof req.body.descricao == undefined || req.body.descricao == null || req.body.descricao.trim().length < 10){
+            erros.push({texto: "Descrição Inválida!"})
+        }
+    
+        if (!req.body.conteudo || typeof req.body.conteudo == undefined || req.body.conteudo == null || req.body.conteudo.trim().length > 20){
+            erros.push({texto: "Conteúdo Inválido!"})
+        }
+
+        if(req.body.categoria == "0") {
+            erros.push({texto: "Categoria Inválida, registre uma no menu."})
+        }
+
+        if (erros.length > 0) {
+            res.render("admin/addpostagem", {erros: erros})
+        } else {
+            const novaPostagem = {
+                titulo: req.body.titulo,
+                slug: req.body.slug,
+                descricao: req.body.descricao,
+                conteudo: req.body.conteudo,
+                categoria: req.body.categoria,
+            }
+
+            new Postagem(novaPostagem).save().then(() => {
+                req.flash("success_msg", "A Postagem foi criada com sucesso!")
+                res.redirect("/admin/postagens")
+            }).catch((err) => {
+                console.log("Ocorreu um erro ao tentar criar uma postagem: "+err)
+                req.flash("error_msg", "Ocorreu um erro ao tentar criar a postagem.")
+                res.redirect("/admin/postagens")
+            })
+
+        }
+
+        
+
+    })
+
 export default router
